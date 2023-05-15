8ab279c9facd95ef5adaa47736c7244f389e667c
Maitreya-today <lessismoreclint@gmail.com>
2023-05-15 11:55:36 -0500

    Remove Quote

--- csharp-hello_world/0-initialize_new_project.sh
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
 
-# Create directory for the new project
-
-
-# Change to the new directory
-
-
 # Initialize a new C# project
-dotnet new console -o "0-new_project"
-
-# Change back to the parent directory
-
+dotnet new console -o 0-new_project
