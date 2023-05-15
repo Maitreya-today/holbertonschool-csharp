5f92ffbdd788d9fce0aaa76b532c3f36916c6382
Maitreya-today <lessismoreclint@gmail.com>
2023-05-15 11:27:03 -0500

    test #3

---
+++ 0-initialize_new_project.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Create directory for the new project
+mkdir 0-new_project
+
+# Change to the new directory
+cd 0-new_project
+
+# Initialize a new C# project
+dotnet new console
+
+# Change back to the parent directory
+cd ..
+
