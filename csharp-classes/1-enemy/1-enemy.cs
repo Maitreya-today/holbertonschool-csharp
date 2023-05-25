2cc915c99058529c44d97b4f2cfb760381dafc1d
Maitreya-today <lessismoreclint@gmail.com>
2023-05-24 20:45:43 -0500

    lets do csharp classes

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Enemies
+{
+    public class Zombie
+    {
+        public int health;
+
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
