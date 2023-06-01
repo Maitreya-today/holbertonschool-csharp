432c4f5033629d8b53f42b4a825cebac96855223
Maitreya-today <lessismoreclint@gmail.com>
2023-06-01 10:54:32 -0500

    update with GPT commads

--- csharp-text_based_interface/InventoryLibrary/BaseClass.cs
+++ csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -1,32 +1,11 @@
 using System;
 
-// namespace InventoryLibrary
-// {
-/// <summary>
-/// This is the base class for the inventory management.
-/// </summary>
-public class BaseClass
+namespace InventoryLibrary
 {
-    public string id { get; }
-    public DateTime date_created { get; }
-    public DateTime date_updated { get; }
-
-    public BaseClass(string id = null, DateTime date_created = default(DateTime), DateTime date_updated = default(DateTime))
+    public class BaseClass
     {
-        if (id == null)
-            this.id = Guid.NewGuid().ToString();
-        else
-            this.id = id;
-
-        if (date_created == default(DateTime))
-            this.date_created = DateTime.Now;
-        else
-            this.date_created = date_created;
-
-        if (date_updated == default(DateTime))
-            this.date_updated = DateTime.Now;
-        else
-            this.date_updated = date_updated;
+        public string id { get; set; }
+        public DateTime date_created { get; set; }
+        public DateTime date_updated { get; set; }
     }
 }
-// }
